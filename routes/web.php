78a48ac4b9b480e1200176841fbc3375d4c65916
namtiennguyen <toilanam97@mail.com>
2020-12-24 19:04:40 +0700

    heroku

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/product',function (){
+Route::get('/',function (){
    return view('index');
 })->name('product.index');
 
