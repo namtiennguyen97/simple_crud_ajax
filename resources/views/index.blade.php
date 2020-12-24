9ae3454e8c2259252beb6421eceae5b8d3861a56
namtiennguyen <toilanam97@mail.com>
2020-12-24 19:03:13 +0700

    heroku

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -53,7 +53,9 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Create Product</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
             <div class="modal-body">
                 <form id="mainForm">
